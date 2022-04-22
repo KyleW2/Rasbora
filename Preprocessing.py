1c6dc462c2752aad64e5ab44d9a29877447c00b1
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-04-21 22:35:31 -0500

    Added FixedTimeHorizon

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,5 +1,50 @@
 from tools.Parsers import CSVParser
 
+"""
+General conventions:
+    classes GoLikeThis
+    functions goLikeThis
+    variables go_like_this
+
+Conventions for labeling:
+    buy = 1
+    hold = 0
+    sell = -1
+
+Terms:
+    look_ahead = number of elements ahead
+    buy_threshold = percent increase required for a buy
+    sell_threshold = precent decrease required for a sell
+"""
+
 class FixedTimeHorizon:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, data: str) -> None:
+        self.data = data
+
+    def label(self, look_ahead: int, buy_threshold: float, sell_threshold: float, save_as: str = None) -> list:
+        # List to store labels
+        labeled = [x for x in range(0, len(self.data) - look_ahead)]
+
+        # Loop thru data
+        for i in range(0, len(self.data) - look_ahead):
+            # If threshold
+            if self.data[i] / self.data[i + look_ahead] >= buy_threshold:
+                labeled[i] = 1
+            elif self.data[i] / self.data[i + look_ahead] <= sell_threshold:
+                labeled[i] = -1
+            else:
+                labeled[i] = 0
+        
+        # Write to csv is save_as
+        if save_as:
+            f = open(save_as, "w")
+            f.write("instance,label\n")
+            for i in range(0, len(labeled)):
+                f.write(f"{self.data[i]},{labeled[i]}\n")
+
+
+if __name__ == "__main__":
+    data = CSVParser("data/AMD.csv").getColumnFloat("Close")
+    
+    fth = FixedTimeHorizon(data)
+    fth.label(5, 1.10, 0.9, "test.csv")
