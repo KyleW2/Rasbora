dbbc267e07ce2a7f3b316fd9f9eb0857a7fe6511
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-05-14 20:44:52 -0500

    Swithced to testing with pytest

---
+++ test_all.py
@@ -0,0 +1,73 @@
+import pytest
+
+from tools.Parsers import CSVParser
+from tools.Preprocessing import FixedTimeHorizon, FixedTimeHorizonMinimized, SimpleMovingAverage, Aggregator
+from tools.Visualization import ScatterPlot, ComparePlot
+
+TEST_DATA_AMD = "data/test/AMD.csv"
+TEST_DATA_INTC = "data/test/INTC.csv"
+
+
+def test_CSVParser():
+    test = CSVParser(TEST_DATA_AMD)
+
+    assert test.getColumnTitles() == ['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']
+    assert type(test.getColumn("Close")) == type([])
+    assert type(test.getColumn("Close")[0]) == type("")
+    assert type(test.getColumnAsFloats("Close")[0]) == type(0.0)
+
+
+def test_FixedTimeHorizon():
+    data = CSVParser(TEST_DATA_AMD).getColumnAsFloats("Close")
+    test = FixedTimeHorizon(data)
+    look_ahead = 5
+    labeled = test.label(look_ahead, 1.10, 0.90)
+    
+    # Might not always be true but works for now
+    assert len(labeled) == len(data) - look_ahead
+    assert 1 in labeled
+    assert 0 in labeled
+    assert -1 in labeled
+
+def test_FixedTimeHorizonMinimized():
+    data = CSVParser(TEST_DATA_AMD).getColumnAsFloats("Close")
+    test = FixedTimeHorizonMinimized(data)
+    look_ahead = 5
+    labeled = test.label(look_ahead, 1.10, 0.90)
+    
+    # Might not always be true but works for now
+    assert len(labeled) == len(data) - look_ahead
+    assert 1 in labeled
+    assert 0 in labeled
+    assert -1 in labeled
+
+def test_SimpleMovingAverage():
+    data = CSVParser(TEST_DATA_AMD).getColumnAsFloats("Close")
+    test = SimpleMovingAverage(data)
+    labeled = test.label()[0]
+
+    assert 1 in labeled
+    assert -1 in labeled
+
+def test_Aggregator():
+    with pytest.raises(ValueError):
+        test = Aggregator()
+        test.combine([1, 2], [[1, 2, 3], [1, 2]])
+
+
+def test_ScatterPlot():
+    data = CSVParser(TEST_DATA_INTC).getColumnAsFloats("Close")
+    look_ahead = 20
+    labels = FixedTimeHorizonMinimized(data).label(look_ahead, 1.10, 0.90)
+    test = ScatterPlot(data[0:len(data) - look_ahead], labels, show = False)
+
+def test_ComparePlot():
+    data = CSVParser(TEST_DATA_INTC).getColumnAsFloats("Close")
+    look_ahead = 20
+    labels1 = FixedTimeHorizonMinimized(data).label(look_ahead, 1.10, 0.90)
+
+    sma = SimpleMovingAverage(data).label()
+    labels2 = sma[0]
+    data2 = sma[1]
+
+    test = ComparePlot(data[0:len(data) - look_ahead], data2, labels1, show = False)
