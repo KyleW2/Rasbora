3e859caebffd8123e3d095f6294bc6e7ff80fbec
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-05-14 20:56:09 -0500

    Fixed testing

--- test_all.py
+++ test_all.py
@@ -53,21 +53,3 @@ def test_Aggregator():
     with pytest.raises(ValueError):
         test = Aggregator()
         test.combine([1, 2], [[1, 2, 3], [1, 2]])
-
-
-def test_ScatterPlot():
-    data = CSVParser(TEST_DATA_INTC).getColumnAsFloats("Close")
-    look_ahead = 20
-    labels = FixedTimeHorizonMinimized(data).label(look_ahead, 1.10, 0.90)
-    test = ScatterPlot(data[0:len(data) - look_ahead], labels, show = False)
-
-def test_ComparePlot():
-    data = CSVParser(TEST_DATA_INTC).getColumnAsFloats("Close")
-    look_ahead = 20
-    labels1 = FixedTimeHorizonMinimized(data).label(look_ahead, 1.10, 0.90)
-
-    sma = SimpleMovingAverage(data).label()
-    labels2 = sma[0]
-    data2 = sma[1]
-
-    test = ComparePlot(data[0:len(data) - look_ahead], data2, labels1, show = False)
