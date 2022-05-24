e7fef46312c2da00661419c40f8a99f9b38d94a4
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-05-24 21:13:45 +0000

    Updated hold color to be transparent

--- Main.py
+++ Main.py
@@ -11,9 +11,8 @@ def run_ScatterPlot():
     labels = FixedTimeHorizonMinimized(data).label(look_ahead, 1.10, 0.90)
     test = ScatterPlot(data[0:len(data) - look_ahead], labels, show = True)
 
-def run_ComparePlot():
+def run_ComparePlot(look_ahead: int):
     data = CSVParser(TEST_DATA_INTC).getColumnAsFloats("Close")
-    look_ahead = 20
     labels1 = FixedTimeHorizonMinimized(data).label(look_ahead, 1.10, 0.90)
 
     mul = 2 / (look_ahead + 1)
@@ -21,7 +20,7 @@ def run_ComparePlot():
     labels2 = ema[0]
     data2 = ema[1]
 
-    test = ComparePlot(data[0:len(data) - look_ahead], data2, labels1, show = True)
+    test = ComparePlot(data[0:len(data) - look_ahead], data2, labels1, save = True)
 
 def AggregateData():
     data = CSVParser("data/test/INTC.csv").getColumnAsFloats("Close")
@@ -29,4 +28,4 @@ def AggregateData():
     gator = Aggregator()
     gator.combine(["index", "price", "sma", "ema"], [[x for x in range(len(data))], data, SimpleMovingAverage(data).label()[1], ExponentialMovingAverage(data, 0.0952).label()[1]], "data/test/aggregated/INTC_agg.csv")
 
-run_ComparePlot()
+run_ComparePlot(20)
--- tools/Visualization.py
+++ tools/Visualization.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import time
 
 class ScatterPlot():
     def __init__(self, data: list, labels: list, show: bool = True) -> None:
@@ -17,18 +18,21 @@ class ScatterPlot():
             plt.show()
 
 class ComparePlot():
-    def __init__(self, data1: list, data2: list, labels: list, show: bool = True) -> None:
+    def __init__(self, data1: list, data2: list, labels: list, show: bool = True, save: bool = False) -> None:
         colors1 = []
         for i in range(0, len(labels)):
             if labels[i] == 1:
-                colors1.append("b")
+                colors1.append((0, 0, 1, 1))
             elif labels[i] == 0:
-                colors1.append("#808080")
+                colors1.append((0.5, 0.5, 0.5, 0.1))
             else:
-                colors1.append("r")
+                colors1.append((1, 0, 0, 1))
 
         plt.scatter([y for y in range(0, len(data1))], data1, c = colors1)
         plt.plot([y for y in range(0, len(data2))], data2)
 
         if show:
             plt.show()
+        
+        if save:
+            plt.savefig(f"plot_{time.time()}.jpg", dpi = 300)
