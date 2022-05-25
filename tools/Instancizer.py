3ef812feb4ba3c04c0935dd22de7c435389c9414
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-05-25 20:02:45 +0000

    Changed instancizer to return array of instance objects

--- tools/Instancizer.py
+++ tools/Instancizer.py
@@ -1,43 +1,24 @@
 import numpy as np
 
+class Instance:
+    def __init__(self, points: list, label: int):
+        self.points = points
+        self.label = label
+    
+    def __str__(self) -> str:
+        return f"Points: {self.points}, Label: {self.label}"
+
 def Instancizer(points: list, labels: list) -> list:
-    # I tried numpy...
-    """
-    # Create array with shape [[[p1, p2, ...], label], [[p1, p2, ...], label], ...]
-    instances = np.zeros(shape = [len(labels), 2])
-
-    for i in range(0, len(labels)):
-        # Create array with shape [[p1, p2, ...], label]
-        instance = np.zeros(shape = (1, 2))
-        np.append(instance, np.zeros(shape = [1, 2]))
-
-        # Append points to sub array
-        for j in range(0, len(points)):
-            np.append(instance[0], points[j][i])
-        
-        # Append label to array
-        np.append(instance, labels[i])
-        
-        # Append instance to instances
-        np.append(instances, instance)
-    """
-
-    # Create array with shape [[[p1, p2, ...], label], [[p1, p2, ...], label], ...]
+    # Create list with shape [Instance(), Instance(), ...]
     instances = []
 
     for i in range(0, len(labels)):
-        # Create array with shape [[p1, p2, ...], label]
-        instance = []
-        instance.append([])
-
-        # Append points to sub array
+        # Append points to temp list
+        p = []
         for j in range(0, len(points)):
-            instance[0].append(points[j][i])
+            p.append(points[j][i])
         
         # Append label to array
-        instance.append(labels[i])
-        
-        # Append instance to instances
-        instances.append(np.array([np.array(instance[0]), instance[1]], dtype = object))
+        instances.append(Instance(np.array(p), labels[i]))
 
-    return np.array(instances)
+    return np.array(instances, dtype = object)
