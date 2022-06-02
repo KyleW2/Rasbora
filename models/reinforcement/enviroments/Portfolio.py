fcfd952ce0d98f2071124f88d92db9ac81efecc1
Kyle Weidner <kyle.j.weidner-1@ou.edu>
2022-06-02 19:05:07 +0000

    Created Portfolio class

---
+++ models/reinforcement/enviroments/Portfolio.py
@@ -0,0 +1,48 @@
+class Portfolio:
+    def __init__(self) -> None:
+        self.positions = {}
+
+    def buy(self, price: float, volume: int) -> None:
+        if price in self.positions.keys():
+            self.positions[price] += volume
+        else:
+            self.positions[price] = volume
+
+    def sell(self, price: float, volume: int) -> bool:
+        if price in self.positions.keys():
+            if self.positions[price] >= volume:
+                self.positions[price] -= volume
+                return True
+            else:
+                return False
+        return False
+    
+    def points(self, current_price: float) -> float:
+        sum = 0
+        change = 0
+        for i in self.positions.keys():
+            sum += self.positions[i]
+            change += ((current_price - i) / i)
+        
+        return 10 * (change / sum)
+    
+    def value(self) -> float:
+        v = 0
+        for i in self.positions.keys():
+            v += self.positions[i] * i
+        
+        return v
+    
+    def dump(self) -> float:
+        v = 0
+        for i in self.positions.keys():
+            v += self.positions[i] * i
+            self.positions[i] = 0
+        
+        return v
+
+if __name__ == "__main__":
+    p = Portfolio()
+    p.buy(10, 1)
+    p.buy(11, 1)
+    print(p.points(15))
